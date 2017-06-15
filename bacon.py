68dd521fcb24f7fd299b95f766b0891cecd29633
thanasis2 <thanasis2@users.noreply.github.com>
2017-06-16 00:18:51 +0300

    Create bacon.py

--- bacon.py
+++ bacon.py
@@ -1,24 +1,100 @@
-
 from urllib.request import urlopen,Request
 from urllib.parse import urlencode
+import json
 
-#import json	# κρατήστε αυτό αν χειρίζεστε αποτελέσματα σε μορφή json...
-#import xml.etree.ElementTree as ET	# ...ή αυτό για αποτελέσματα σε xml
+endpoint = "http://data.linkedmdb.org/sparql?"
+
+actorname = input("Type the actor first name for your query:")
+
+sparqlq = """
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX movie: <http://data.linkedmdb.org/resource/movie/>
+SELECT ?actor_id ?actor_name WHERE {
+?actor movie:actor_actorid ?actor_id .
+?actor movie:actor_name ?actor_name .
+filter(regex(?actor_name, '"""+actorname+""" ',"i"))
+}
+"""
+
+# params sent to server
+params = { 'query': sparqlq }
+# create appropriate param string
+paramstr = urlencode(params)
+
+# create GET http request object with params appended
+req = Request(endpoint+paramstr)
+# request specific content type
+req.add_header('Accept','application/sparql-results+json')
+# dispatch request
+page = urlopen(req)
+# get results and close
+text = page.read().decode('utf-8')
+page.close()
+
+# convert to json object
+jso = json.loads(text)
+
+actors = []
+
+# iterate over results
+for binding in jso['results']['bindings']:
+    # for every column in binding
+    for bname,bcontent in binding.items():
+        aname = ""
+        if bname == "actor_id":
+            aid = bcontent['value']
+        elif bname == "actor_name":
+            aname = bcontent['value']
+            actors.append([aid, aname])
+
+for actor in actors:
+    print("%s -> %s" % (actor[0], actor[1]))
+
+aid = int(input("Type the actor id for your selection: "))
+
+sparqlq = """
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX movie: <http://data.linkedmdb.org/resource/movie/>
+SELECT ?actname ?mtitle WHERE {
+?film movie:filmid ?mid.
+?film movie:actor ?mactor.
+?film rdfs:label ?mtitle.
+?film movie:actor <http://data.linkedmdb.org/resource/actor/"""+str(aid)+"""> .
+?mactor movie:actor_name ?actname.
+?mactor movie:actor_actorid ?aid.
+FILTER(?aid!="""+str(aid)+""")
+}
+"""
 
 
-# ζητήστε ένα μικρό όνομα από τον χρήστη
+# params sent to server
+params = { 'query': sparqlq }
+# create appropriate param string
+paramstr = urlencode(params)
 
+# create GET http request object with params appended
+req = Request(endpoint+paramstr)
+# request specific content type
+req.add_header('Accept','application/sparql-results+json')
+# dispatch request
+page = urlopen(req)
+# get results and close
+text = page.read().decode('utf-8')
+page.close()
 
-# με την πρώτη ερώτηση SPARQL ανακτήστε λίστα ηθοποιών (και τα URIs τους) με το όνομα αυτό
+# convert to json object
+jso = json.loads(text)
 
+actors = []
+# iterate over results
+for binding in jso['results']['bindings']:
+    # for every column in binding
+    for bname,bcontent in binding.items():
+        if bname == "actname":
+            actname = bcontent['value']
+        elif bname == "mtitle":
+            mtitle = bcontent['value']
+            actors.append([actname, mtitle])
 
-# ζητήστε από τον χρήστη να επιλέξει ηθοποιό
-
-
-# με τη δεύτερη ερώτηση SPARQL βρείτε τους συμπρωταγωνιστές/ταινίες
-
-
-# τυπώστε ονόματα και τίτλους ταινιών
-
-
-
+for actor in actors:
+    print("   %s -> %s" % (actor[0], actor[1]))
